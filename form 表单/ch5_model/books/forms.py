4607d28481e210db7f7826324e9a644f35427c84
lzhan <zhanlingofhit@gmail.com>
2015-12-28 22:45:35 +0800

    自定义form检验：clean_xx 将在 默认检验完成后自动调用，并且必须返回呗校验的值，否则原始的值将丢失。

--- form 表单/ch5_model/books/forms.py
+++ form 表单/ch5_model/books/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
 class AddForm(forms.Form):
-    a = forms.IntegerField()
+    a = forms.IntegerField("max_length=1")
     b = forms.IntegerField()
     c = forms.IntegerField()
     d = forms.CharField()
+
+    def clean_a(self):
+        a = self.cleaned_data['a']
+        if a < 0:
+            raise forms.ValidationError("a must be a positive number")
+        return a
