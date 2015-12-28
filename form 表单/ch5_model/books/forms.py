a2bf8ce395ac57339075b943a8d0fed01deb7a1b
lzhan <zhanlingofhit@gmail.com>
2015-12-28 22:57:34 +0800

    update

--- form 表单/ch5_model/books/forms.py
+++ form 表单/ch5_model/books/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
 class AddForm(forms.Form):
-    a = forms.IntegerField("max_length=1")
+    a = forms.IntegerField()
     b = forms.IntegerField()
     c = forms.IntegerField()
     d = forms.CharField()
 
-    def clean_a(self):
+    def clean_a(self): #selfdefined input check
         a = self.cleaned_data['a']
         if a < 0:
             raise forms.ValidationError("a must be a positive number")
